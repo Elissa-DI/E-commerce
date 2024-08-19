f9db4dad4148227a6a667792fb8a3c0ff9cab0b4
Elissa-DI <elissadusabe@gmail.com>
2024-08-19 10:02:55 +0200

    Added getUser, updateUser, and deleteUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,3 +78,113 @@ export const loginUser = async (req, res) => {
         res.status(500).send('Server error');
     }
 };
+
+export const getUser = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        });
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({
+                    msg: 'User not found!'
+                });
+        }
+
+        res
+            .status(200)
+            .json({
+                msg: "User profile retrieved successfully",
+                user
+            });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { name, email } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (existingUser && existingUser.id !== userId) {
+            return res
+                .status(400)
+                .json({
+                    msg: 'Email is already in use by another account.'
+                });
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                name: name,
+                email: email
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        });
+
+        res
+            .status(200)
+            .json({
+                msg: 'Profile updated successfully',
+                user: updatedUser
+            });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        await prisma.user.delete({
+            where: {
+                id: userId
+            }
+        });
+
+        res
+            .status(200)
+            .json({
+                msg: 'Profile deleted successfully'
+            });
+    } catch (error) {
+        console.error(error.message);
+        res
+            .status(500)
+            .send('Server error');
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import { registerUser, loginUser, getUser, updateUser, deleteUser } from '../controllers/authController.js';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+//users
+router.get('/profile', auth, getUser);
+router.put('/profile', auth, updateUser);
+router.delete('/profile', auth, deleteUser);
 
 export default router;
